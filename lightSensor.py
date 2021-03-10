db4b41813015cccdf04fc39715c631dcc146e75a
tdrisco <tdrisco@clemson.edu>
2021-03-10 12:31:30 -0800

    light sensor test code

--- lightSensor.py
+++ lightSensor.py
@@ -7,8 +7,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.IN)
 
 
-def LIGHT(PIN):
-        print("Light On")
+#def LIGHT(PIN):
+        #print("Light On")
 
 print("Light Sensor Test. Use ctrl +c to stop")
 time.sleep(2)
@@ -16,11 +16,15 @@ print("Ready")
 
 try:
 
-    GPIO.add_event_detect(PIN, GPIO.RISING, callback=LIGHT)  # let us know when the pin goes HIGH or LOW
+    #GPIO.add_event_detect(PIN, GPIO.RISING, callback=LIGHT)  # let us know when the pin goes HIGH or LOW
     # infinite loop
     while True:
-            time.sleep(1)
-            print("Light Off")
+            if GPIO.input(PIN):
+                print("Light On \u263e")
+                time.sleep(1)
+            else:
+                print("Light Off \u263c")
+                time.sleep(1);
 
 except KeyboardInterrupt:
                 print("Quit")
